94d71e31e6ff5b40dc09a58b6333738d357cfcaa
qaisa076 <qais.ahmad@ada.ac.uk>
2024-10-23 13:48:42 +0000

    All tests passed

--- dfp.js
+++ dfp.js
@@ -6,23 +6,21 @@ function parseFile(inputFile, outputFile, delimiter = ';') {
   if (!fs.existsSync(inputFile)) {
     return -1;
   }
+  //Deleting any existing output file
   if (fs.existsSync(outputFile)) {
     fs.unlinkSync(outputFile);
   }
   const data = fs.readFileSync(inputFile, "utf-8");
+  //Getting rid of any headers while seperating reviews
   const lines = data.split(/\n/).slice(1); 
-
   let totalRecords = 0;
+
   for (let line of lines) {
     const trimmedLine = line.trim();
-    if (!trimmedLine) continue;
-
-    const elements = trimmedLine.split(delimiter);
-    if (elements.length < 2) continue; 
-
-    const review = elements[0].trim();
+    const elements = trimmedLine.split(delimiter); //Cutting off at the semi colon
+    const review = elements[0].trim(); 
     const sentiment = elements[1].trim();
-    const shortReview = review.substring(0, 20);
+    const shortReview = review.substring(0, 20);//Making reviews 20 characters max
     const outputLine = `${sentiment}${delimiter}${shortReview}\n`;
     fs.appendFileSync(outputFile, outputLine);
     totalRecords++;
@@ -30,8 +28,6 @@ function parseFile(inputFile, outputFile, delimiter = ';') {
   return totalRecords; 
 }
 console.log(parseFile(inputFile, outputFile));
-
-
 // Leave this code here for the automated tests
 module.exports = {
   parseFile,
