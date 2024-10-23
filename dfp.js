96af183e11d5f1729b5eba5257d8260a07b4d9be
qaisa076 <qais.ahmad@ada.ac.uk>
2024-10-23 09:18:00 +0000

    Deleting existing output files

--- dfp.js
+++ dfp.js
@@ -1,6 +1,13 @@
 const fs = require('fs');
 
+const inputFile = "outputfile.csv"
+const outputFile = "datafile.csv"
+
+
 function parseFile (indata, outdata, delimiter = ';') {
+  if (fs.existsSync(outdata)) {
+    fs.unlinkSync(outdata)
+  }
 
 }
 
